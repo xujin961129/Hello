a814a931aa02710feabc20caca2dca9893fd2eda
xujin <xujin19961129@163.com>
2019-07-09 15:17:36 +0800

    add main by xj

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+struct student
+{
+    int id;
+    string name;
+};
+class student
+{
+    int id;
+};
+int main()
+{
+    cout << "Hello World!" << endl;
+    return 0;
+}
